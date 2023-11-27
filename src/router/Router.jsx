208beb5c419b47f81e9a961a63f14b66e9bdebb9
Luis Restrepo <luchoposada555@gmail.com>
2023-11-27 11:30:40 -0400

    Orden de rutas

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,27 +4,65 @@ import MiComponente from "../components/miComponente/MiComponente";
 import MiContador from "../components/miComponente/miContador/MiContador";
 import MisProductos from "../components/miComponente/MisProductos/MisProductos";
 import DetallesProductos from "../components/miComponente/DetallesProducto/DetallesProductos";
-import Layout from "../components/layout/Layout.jsx"
+import Layout from "../components/layout/Layout.jsx";
 import MiContadorConUseReducer from "../components/MiContadorConUseReducer/MiContadorConUseReducer.jsx";
 import MiToDoList from "../components/MiToDoList/MiToDoList.jsx";
 
-
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/micomponente" element={<MiComponente />} />
-          <Route index path="/" element={<MiComponente />} />
-          <Route path="/micontador" element={<MiContador />} />
-          <Route path="/misproductos" element={<MisProductos />} />
-          <Route path="/misproductos/:id" element={<DetallesProductos />} />
-          <Route path="/miContadorUseReducer" element={<MiContadorConUseReducer />} />
-          <Route path="/miTodoList" element={<MiToDoList />} />
+  const [userLogged, userLoggedDispatch] = useReducer(
+    userLoggedReducer,
+    userLoggedInitial
+  );
 
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  const [postState, postDispatch] = useReducer(postReducer, postInitial);
+
+  const globalState = {
+    userLogged: {
+      userLogged,
+      userLoggedDispatch,
+    },
+    postReducerInfo: {
+      postState,
+      postDispatch,
+    },
+  };
+  return (
+    <AppContext.Provider value={globalState}>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                element={
+                  <PrivateRoutes isAuthenticate={userLogged.isAuthenticated} />
+                }
+              >
+                <Route path="/miTodoList" element={<MiToDoList />} />
+              </Route>
+              <Route
+                element={
+                  <PublicRoutes isAuthenticate={userLogged.isAuthenticated} />
+                }
+              >
+                <Route index path="/" element={<MiComponente />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/micomponente" element={<MiComponente />} />
+                <Route path="/micontador" element={<MiContador />} />
+                <Route path="/misproductos" element={<MisProductos />} />
+                <Route
+                  path="/misproductos/:id"
+                  element={<DetallesProductos />}
+                />
+                <Route
+                  path="/miContadorUseReducer"
+                  element={<MiContadorConUseReducer />}
+                />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </AppContext.Provider>
   );
 };
 
